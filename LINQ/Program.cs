32658941c0ee1389f702056fd611e1404e4d863a
Amy Galbicsek <acgalbicsek@gmail.com>
2023-02-02 16:53:44 -0900

    GameList Complete

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            var GameList = new List<string>()
+            {
+                "Oblivion", "Skyrim", "Child Of Light", "Zoo Tycoon", "Rayman"
+            };
+
+
+
+            
+
+            var result = GameList.OrderBy(NameLength => NameLength.Count()).ToList();
+
+
+           foreach(var Game in result)
+            {
+                Console.WriteLine(Game);
+            }
+
         }
+
+        
     }
 }
